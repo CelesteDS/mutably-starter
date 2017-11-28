f4c18baec941854a3664bae39e79546a6f645155
Celeste De Santiago <celestedaphne@gmail.com>
2017-11-27 17:10:22 -0800

    First fetch implemented to get books and update the DOM

--- public/script.js
+++ public/script.js
@@ -1,7 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('.get-books').on('click', function() {
+    console.log('in click ');
+    fetch('http://mutably.herokuapp.com/books', { method: 'get' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(res) {
+      const bookNodes = [];
+      Object.keys(res.books).forEach((el, ind) => {
+        bookNodes.push($(`<li>Title:${res.books[ind].title}, Author:${res.books[ind].author}</li>`));
+      });
+      bookNodes.forEach((node) => {
+        $('.list-group').append(node);
+      });
+    })
+  });
   // code in here
 
 });
